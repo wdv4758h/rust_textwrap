c5f94f96aa2493718e72d15e51ecc7939d630ad1
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2016-06-23 21:58:23 +0800

    Remove some unneeded .to_string()
    * use index operator and reference instead of a lot of .to_string()
    * check empty str

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ use regex::Regex;
 
 /// Remove any common leading whitespace from every line in `text`.
 pub fn dedent(text: &str) -> String {
+    if text.is_empty() {
+        return text.to_owned();
+    }
 
     // replace "^[ \t]+$" with "" to clean empty lines
     // findall "(^[ \t]*)(?:[^ \t\n])" to count leading whitespaces
@@ -22,35 +25,44 @@ pub fn dedent(text: &str) -> String {
     }
 
     let text = WHITESPACE_ONLY_RE.replace_all(text, "");
-    let mut indents_idx = LEADING_WHITESPACE_RE.find_iter(text.as_str());
 
-    let (start, end) = indents_idx.next().unwrap(); // may fail when nothing
-    let mut margin = text[start..end].to_string();
+    {
+        // this will borrow text
+        let mut indents_idx = LEADING_WHITESPACE_RE.find_iter(text.as_str());
 
-    for (start, end) in indents_idx {
-        let indent = text[start..end].to_string();
-        if margin.starts_with(indent.as_str()) {
-            margin = indent.to_string();
-        } else if !indent.starts_with(margin.as_str()) {
-            // find longest common whitespaces
-            for (i, (x, y)) in margin.to_string()
-                                     .chars()
-                                     .zip(indent.chars())
-                                     .enumerate() {
-                if x != y {
-                    margin = margin[..i].to_string();
-                    break;
+        let (mut pre_start, mut pre_end) = indents_idx.next().unwrap();
+        pre_end = pre_end-1;
+
+        for (start, end) in indents_idx {
+            let end = end-1;
+            let margin = &text[pre_start..pre_end];  // FIXME, borrow every time
+            let indent = &text[start..end];
+
+            if indent.starts_with(margin) {
+            } else if margin.starts_with(indent) {
+                pre_end = pre_start + (end - start);
+            } else if !indent.starts_with(margin) {
+                // find longest common whitespaces
+                for (i, (x, y)) in margin.chars()
+                                         .zip(indent.chars())
+                                         .enumerate() {
+                    if x != y {
+                        pre_end = pre_start + i;
+                        break;
+                    }
                 }
             }
         }
+
+        let margin = &text[pre_start..pre_end];     // final borrow
+
+        if !margin.is_empty() {
+            let mut pattern = r"(?m)^".to_string();
+            pattern.push_str(margin);
+            let re = Regex::new(pattern.as_str()).unwrap();
+            return re.replace_all(text.as_str(), "");
+        }
     }
 
-    if !margin.is_empty() {
-        let mut pattern = r"(?m)^".to_string();
-        pattern.push_str(margin.as_str());
-        let re = Regex::new(pattern.as_str()).unwrap();
-        return re.replace_all(text.as_str(), "");
-    }
-
-    text.to_string()
+    text
 }
